6ba2b90356122a7ce487578effb7afcef67c9133
Valeriya <valeriya-fursenko@yandex.ru>
2016-10-24 01:14:30 +0200

    Forgot to delete one print during the merging

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -13,7 +13,6 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you now? :)");
-		System.out.println("Hello ASE2016 - how are you? :)");
 		HelloUser l = new HelloUser();
 		l.greetUser();
 	}
